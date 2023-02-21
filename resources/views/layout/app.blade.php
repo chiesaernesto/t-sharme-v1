4a7cd100c418913920199ec29fbcf54ea1806c3f
chiesaernesto <beijaminangel@gmail.com>
2023-02-22 00:02:46 +0100

    second commit

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  @vite('resources/css/app.css')
+  <title>@yield('title')</title>
+</head>
+<body>
+
+  <div class="app"> @yield('content') </div>
+
+</body>
+</html>
